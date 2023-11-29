ec3424a2699b06506c9223120abca33e8084e400
mjazvic <mjazvic@playnirvana.com>
2023-11-29 11:14:21 +0100

    .

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, } from '@angular/router';
+import { CanActivate, Router, } from '@angular/router';
 import {UserService} from "./services/user.service";
 import {Observable, tap} from "rxjs";
 
@@ -14,10 +14,7 @@ export class AuthGuard implements CanActivate {
   ) {
   }
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean | Observable<boolean> {
+  canActivate():  Observable<boolean> {
     return this.userService.isAuthorized().pipe(
       tap((isAuthorized) => {
         if (isAuthorized) {
